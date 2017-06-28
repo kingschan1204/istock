07c8b6caffadceb8e85080eb686310146f36f1c8
kings.chan <kings.chan@qq.com>
2017-06-28 13:33:33 +0800

    加入logback日志记录

--- src/main/java/io.github.kingschan1204.istock/Application.java
+++ src/main/java/io.github.kingschan1204.istock/Application.java
@@ -1,8 +1,7 @@
-package com.kingschan1204.istock;
+package io.github.kingschan1204.istock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@ComponentScan({"com.kingschan1204.istock"})
+@ComponentScan({"io.github.kingschan1204.istock"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/io.github.kingschan1204.istock/controller/SpringmvcController.java
+++ src/main/java/io.github.kingschan1204.istock/controller/SpringmvcController.java
@@ -1,4 +1,4 @@
-package com.kingschan1204.istock.controller;
+package io.github.kingschan1204.istock.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
