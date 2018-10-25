62c10f8fd52803cf409855445fe91286d9c82099
kingschan <kings.chan@qq.com>
2018-10-25 09:56:01 +0800

    代码价格更新频率调整

--- src/main/java/io.github.kingschan1204.istock/module/startup/InitQuartzTaskRunner.java
+++ src/main/java/io.github.kingschan1204.istock/module/startup/InitQuartzTaskRunner.java
@@ -24,14 +24,14 @@ public class InitQuartzTaskRunner implements ApplicationRunner, Ordered {
                 "sinaPriceTask-trigger",
                 "sinaPriceTask-trigger-group",
                 SinaStockPriceTask.class,
-                "0 0/1 * * * ?");
+                "0/30 * * * * ?");
 
         quartzManager.addJob("tencentPriceTask",
                 "tencentPriceTask-group",
                 "tencentPriceTask-trigger",
                 "tencentPriceTask-trigger-group",
                 TencentStockPriceTask.class,
-                "0 0/1 * * * ?");
+                "0/30 * * * * ?");
 
         quartzManager.addJob("stockCodeTask",
                 "stockCodeTask-group",
