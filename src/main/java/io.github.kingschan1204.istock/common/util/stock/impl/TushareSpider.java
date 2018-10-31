79cb42698e4b7a098a3dd5c50ef8f160d3e4b803
kingschan <kings.chan@qq.com>
2018-10-31 18:14:54 +0800

    上市公司信息抓取

--- src/main/java/io.github.kingschan1204.istock/common/util/stock/impl/TushareSpider.java
+++ src/main/java/io.github.kingschan1204.istock/common/util/stock/impl/TushareSpider.java
@@ -28,31 +28,57 @@ public class TushareSpider  {
     private RestTemplate restTemplate;
     final String api="http://api.tushare.pro";
 
+
+    /**
+     * post 方式提交
+     * @param params
+     * @return
+     */
+    String post(JSONObject params){
+        HttpHeaders headers = new HttpHeaders();
+        //定义请求参数类型，这里用json所以是MediaType.APPLICATION_JSON
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        params.put("token", tuToken);
+        HttpEntity<String> formEntity = new HttpEntity<String>(params.toString(), headers);
+        String result = restTemplate.postForObject(api, formEntity, String.class);
+        return result;
+    }
+
+
     /**
      * 返回已上市的A股代码
      * @return TS代码,股票代码,股票名称,所在地域,所属行业,股票全称,市场类型 （主板/中小板/创业板）,上市状态： L上市 D退市 P暂停上市,上市日期
      */
     public JSONArray getStockCodeList(){
-        HttpHeaders headers = new HttpHeaders();
-        //定义请求参数类型，这里用json所以是MediaType.APPLICATION_JSON
-        headers.setContentType(MediaType.APPLICATION_JSON);
         JSONObject json = new JSONObject();
         //接口名称
         json.put("api_name","stock_basic");
         //只取上市的
         json.put("params",JSON.parse("{'list_status':'L'}"));
         json.put("fields","ts_code,symbol,name,area,industry,fullname,market,list_status,list_date");
-        json.put("token", tuToken);
-        HttpEntity<String> formEntity = new HttpEntity<String>(json.toString(), headers);
-        String result = restTemplate.postForObject(api, formEntity, String.class);
+        String result = post(json);
         JSONObject datas= JSON.parseObject(result);
         JSONArray items =datas.getJSONObject("data").getJSONArray("items");
         return items;
     }
 
+    /**
+     *获取上市公司基础信息
+     * @return
+     */
+    public JSONArray getStockCompany(){
+        JSONObject json = new JSONObject();
+        //接口名称
+        json.put("api_name","stock_company");
+        json.put("fields","ts_code,chairman,manager,secretary,reg_capital,setup_date,province,city,introduction,website,email,office,employees,main_business,business_scope");
+        String result = post(json);
+        JSONObject datas= JSON.parseObject(result);
+        JSONArray items =datas.getJSONObject("data").getJSONArray("items");
+        return items;
+    }
 
     public static void main(String[] args) {
-        System.out.println(new TushareSpider().getStockCodeList());
+        System.out.println(new TushareSpider().getStockCompany());
 
     }
 }
---
+++ src/main/java/io.github.kingschan1204.istock/module/maindata/services/StockCompanyService.java
@@ -0,0 +1,41 @@
+package io.github.kingschan1204.istock.module.maindata.services;
+
+import com.alibaba.fastjson.JSONArray;
+import io.github.kingschan1204.istock.common.util.stock.impl.TushareSpider;
+import io.github.kingschan1204.istock.module.maindata.po.StockCompany;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author chenguoxiang
+ * @create 2018-10-31 16:42
+ **/
+@Service
+public class StockCompanyService {
+
+        Logger log = LoggerFactory.getLogger(StockCompanyService.class);
+        @Autowired
+        private TushareSpider tushareSpider;
+        @Autowired
+        private MongoTemplate mongoTemplate;
+
+    /**
+     * 代码列表刷新
+     */
+    public void refreshStockCompany(){
+        List<StockCompany> list = new ArrayList<StockCompany>();
+        JSONArray rows =tushareSpider.getStockCodeList();
+        for (int i = 0; i < rows.size(); i++) {
+            list.add(new StockCompany(rows.getJSONArray(i)));
+        }
+        //先删除再新增
+        mongoTemplate.dropCollection(StockCompany.class);
+        mongoTemplate.insertAll(list);
+    }
+}
