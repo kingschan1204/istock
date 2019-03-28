545119146952fbea7cc76d3dcbb0aad48a2d0e2d
kingschan <kings.chan@qq.com>
2019-03-28 12:51:18 +0800

    trading spider

--- src/main/java/io.github.kingschan1204.istock/module/spider/AbstractSpider.java
+++ src/main/java/io.github.kingschan1204.istock/module/spider/AbstractSpider.java
@@ -1,29 +1,28 @@
 package io.github.kingschan1204.istock.module.spider;
 
-import io.github.kingschan1204.istock.common.util.stock.StockDateUtil;
 import io.github.kingschan1204.istock.module.spider.entity.WebPage;
+import io.github.kingschan1204.istock.module.spider.util.JsoupUitl;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import java.io.IOException;
+
 import java.net.Proxy;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * 抽象爬虫
+ *
  * @author chenguoxiang
  * @create 2019-03-07 9:25
  **/
 @NoArgsConstructor
 @AllArgsConstructor
 @Slf4j
-public abstract class AbstractSpider<T> implements Runnable,ISpider{
+public abstract class AbstractSpider<T> implements Runnable, ISpider {
     protected ConcurrentLinkedQueue<T> queue;
-    protected Map<String,String> cookie;
+    protected Map<String, String> cookie;
     protected String referer;
     protected String pageUrl;
     protected Integer timeOut;
@@ -37,46 +36,20 @@ public abstract class AbstractSpider<T> implements Runnable,ISpider{
 
     @Override
     public WebPage crawlPage() {
-        WebPage webPage=null;
-        Connection connection =Jsoup.connect(pageUrl)
-                .timeout(null==timeOut?8000:timeOut)
-                .method(null==method? Connection.Method.GET:method);
-        if(null!=ignoreContentType){
-            connection.ignoreContentType(ignoreContentType);
-        }
-        if(null!=ignoreHttpErrors){
-            connection.ignoreHttpErrors(ignoreHttpErrors);
-        }
-       if(null!=referer){
-           connection.referrer(referer);
-       }
-       if(null!=proxy){
-           connection.proxy(proxy);
-       }
-       if(null!=cookie){
-           connection.cookies(cookie);
-       }
-        try {
-            Long start=System.currentTimeMillis();
-            log.info(pageUrl);
-            Connection.Response response =connection.execute();
-            Document document=response.parse();
-            webPage=new WebPage(System.currentTimeMillis()-start,pageUrl,document,document.html());
-            return webPage;
-        } catch (IOException e) {
-            log.error("crawlPage {} {}",pageUrl,e);
-            e.printStackTrace();
-        }
-        return null;
+        return JsoupUitl.getWebPage(
+                pageUrl, method,
+                timeOut, useAgent, referer,
+                cookie, proxy,
+                ignoreContentType, ignoreHttpErrors);
     }
 
     @Override
     public void run() {
-        try{
-            WebPage webPage=crawlPage();
+        try {
+            WebPage webPage = crawlPage();
             parsePage(webPage);
-        }catch (Exception ex){
-            log.error("page process error {} {}",pageUrl,ex);
+        } catch (Exception ex) {
+            log.error("page process error {} {}", pageUrl, ex);
         }
     }
 }
