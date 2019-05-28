26936ef16fb77301480bcb288d9811f056bac9a1
kingschan <kings.chan@qq.com>
2019-05-28 13:51:18 +0800

    修正在服务器上cpu占用过高问题

--- src/main/java/io.github.kingschan1204.istock/module/spider/crawl/index/IndexCrawlJob.java
+++ src/main/java/io.github.kingschan1204.istock/module/spider/crawl/index/IndexCrawlJob.java
@@ -6,16 +6,14 @@ import io.github.kingschan1204.istock.module.maindata.po.Stock;
 import io.github.kingschan1204.istock.module.maindata.po.StockCodeInfo;
 import io.github.kingschan1204.istock.module.maindata.services.StockCodeInfoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author chenguoxiang
@@ -26,6 +24,7 @@ public class IndexCrawlJob implements Runnable {
 
     private ScheduledExecutorService scheduledExecutorService ;
     private ScheduledExecutorService scheduledExecutorService2 ;
+    ConcurrentLinkedQueue<Stock> stockQueue = new ConcurrentLinkedQueue<>();
 
     //scheduleAtFixedRate 也就是规定频率为1h，那么好，A任务开始执行，过来一个小时后，不管A是否执行完，都开启B任务
     //scheduleWithFixedDealy却是需要在A任务执行完后，在经过1小时后再去执行B任务；
@@ -40,12 +39,22 @@ public class IndexCrawlJob implements Runnable {
         Thread.currentThread().interrupt();
     }
 
+    private void top(){
+        ThreadPoolExecutor threadPoolExecutor =(ThreadPoolExecutor)scheduledExecutorService;
+        log.info("总线程数{},活动线程数{},执行完成线程数{},排队线程数{},数据队列数{}",
+                threadPoolExecutor.getTaskCount(),
+                threadPoolExecutor.getActiveCount(),
+                threadPoolExecutor.getCompletedTaskCount(),
+                threadPoolExecutor.getQueue().size(),
+                stockQueue.size()
+                );
+    }
 
     @Override
     public void run() {
         StockCodeInfoService stockCodeInfoService= SpringContextUtil.getBean(StockCodeInfoService.class);
         MongoTemplate template=SpringContextUtil.getBean(MongoTemplate.class);
-        ConcurrentLinkedQueue<Stock> stockQueue = new ConcurrentLinkedQueue<>();
+
         //sz
         List<StockCodeInfo> sz_codes = stockCodeInfoService.getSZStockCodes();
         List<String> list = new ArrayList<>();
@@ -84,13 +93,40 @@ public class IndexCrawlJob implements Runnable {
         }
 
 
-        scheduledExecutorService2.execute(new Runnable() {
+        scheduledExecutorService2.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                top();
+                int index=stockQueue.size()>1000?1000:stockQueue.size();
+                if(index==0){return;}
+                //这里的BulkMode.UNORDERED是个枚举，，，collectionName是mongo的集合名
+                BulkOperations ops = template.bulkOps(BulkOperations.BulkMode.UNORDERED, "stock");
+                for (int i = 0; i < index; i++) {
+                    Stock stock = stockQueue.poll();
+                    ops.updateOne(new Query(Criteria.where("_id").is(stock.getCode())), new Update()
+                            .set("_id", stock.getCode())
+                            .set("type", stock.getType())
+                            .set("name", stock.getName())
+                            .set("price", stock.getPrice())
+                            .set("yesterdayPrice", stock.getYesterdayPrice())
+                            .set("fluctuate", stock.getFluctuate())
+                            .set("todayMax", stock.getTodayMax())
+                            .set("todayMin", stock.getTodayMin())
+                            .set("priceDate", stock.getPriceDate()));
+                }
+                //循环插完以后批量执行提交一下ok！
+                ops.execute();
+
+            }
+        }, 0, 2, TimeUnit.SECONDS);
+       /* scheduledExecutorService2.execute(new Runnable() {
             @Override
             public void run() {
                 while (true && !Thread.currentThread().isInterrupted()) {
                     Stock stock = stockQueue.poll();
+                    top();
                     if (null != stock) {
-                        log.info("stockQueue size {} : ",stockQueue.size());
+                        log.info("stockQueue size {} : ",stockQueue.size(),scheduledExecutorService );
                         template.upsert(
                                 new Query(Criteria.where("_id").is(stock.getCode())),
                                 new Update()
@@ -108,6 +144,6 @@ public class IndexCrawlJob implements Runnable {
                     }
                 }
             }
-        });
+        });*/
     }
 }
