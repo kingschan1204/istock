9cb4a06a647348e0a1f8c573df29f00006f3de5a
kingschan <kings.chan@qq.com>
2019-07-25 09:13:44 +0800

    正式去掉quartz

--- src/main/java/io.github.kingschan1204.istock/common/startup/InitQuartzTaskRunner.java
+++ src/main/java/io.github.kingschan1204.istock/common/startup/InitQuartzTaskRunner.java
@@ -1,38 +1,19 @@
 package io.github.kingschan1204.istock.common.startup;
 
-import io.github.kingschan1204.istock.common.util.quartz.QuartzManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.Ordered;
 
 /**
- *
  * @author chenguoxiang
  * @create 2018-07-13 15:12
  **/
 public class InitQuartzTaskRunner implements ApplicationRunner, Ordered {
 
-    @Autowired
-    private QuartzManager quartzManager;
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
 
-       /* quartzManager.addJob("stockDividendTask",
-                "stockDividendTask-group",
-                "stockDividendTask-trigger",
-                "stockDividendTask-trigger-group",
-                StockDividendTask.class,
-                "6 * * * * ?");*/
-
-
-        /*quartzManager.addJob("hisRepoartTask",
-                "hisRepoartTask-group",
-                "hisRepoartTask-trigger",
-                "hisRepoartTask-trigger-group",
-                ThsHisYearReportTask.class,
-                "6 * * * * ?");*/
 
     }
 
