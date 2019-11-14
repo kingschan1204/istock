6311f89f0e491323f54c884c25cfe4a63799a1b4
chenguoxiang <chenguoxiang@chalco-steering.com>
2019-11-14 15:42:34 +0800

    空指针修正

--- src/main/java/io.github.kingschan1204.istock/module/spider/crawl/info/ThsInfoSpider.java
+++ src/main/java/io.github.kingschan1204.istock/module/spider/crawl/info/ThsInfoSpider.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 /**
  * 同花顺Info信息爬虫
+ *
  * @author chenguoxiang
  * @create 2019-03-26 14:10
  **/
@@ -32,35 +33,36 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
     //当前要处理的代码
     private StockCodeInfo currentCodeInfo;
 
-    public ThsInfoSpider(){
-        this.useAgent=SpringContextUtil.getProperties("spider.useagent");
+    public ThsInfoSpider() {
+        this.useAgent = SpringContextUtil.getProperties("spider.useagent");
     }
 
-    public MongoTemplate getMongoTemp(){
+    public MongoTemplate getMongoTemp() {
         return SpringContextUtil.getBean(MongoTemplate.class);
     }
 
     /**
      * 得到当前要处理的代码
+     *
      * @return
      */
-    private void getCodeInfo(){
+    private void getCodeInfo() {
         Integer dateNumber = Integer.valueOf(TradingDateUtil.getDateYYYYMMdd());
         Criteria cr = new Criteria();
         Criteria c1 = Criteria.where("infoDate").lt(dateNumber);
         Criteria c2 = Criteria.where("infoDate").exists(false);
-        Query query = new Query(cr.orOperator(c1,c2));
+        Query query = new Query(cr.orOperator(c1, c2));
         query.limit(1);
         List<StockCodeInfo> list = getMongoTemp().find(query, StockCodeInfo.class);
-        if(null!=list&&list.size()>0){
-            this.currentCodeInfo=list.get(0);
-        }else{
+        if (null != list && list.size() > 0) {
+            this.currentCodeInfo = list.get(0);
+        } else {
             try {
                 log.info("stock info 全部更新完毕，关闭更新线程！");
                 ITimeJobFactory.getJob(ITimeJobFactory.TIMEJOB.INFO).execute(ITimerJob.COMMAND.STOP);
             } catch (Exception e) {
                 e.printStackTrace();
-                log.error("{}",e);
+                log.error("{}", e);
             }
         }
     }
@@ -68,20 +70,20 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
     @Override
     public WebPage crawlPage() {
         getCodeInfo();
-        if(null==currentCodeInfo){
+        if (null == currentCodeInfo) {
             return null;
         }
-        if(!currentCodeInfo.getCode().matches("\\d+")){
-         log.error("{} 代码错误，不是有效的代码！",currentCodeInfo.getCode());
-         return null;
+        if (!currentCodeInfo.getCode().matches("\\d+")) {
+            log.error("{} 代码错误，不是有效的代码！", currentCodeInfo.getCode());
+            return null;
         }
-        this.pageUrl= String.format("http://basic.10jqka.com.cn/%s/", currentCodeInfo.getCode());
+        this.pageUrl = String.format("http://basic.10jqka.com.cn/%s/", currentCodeInfo.getCode());
         return super.crawlPage();
     }
 
     @Override
     public void parsePage(WebPage webPage) throws Exception {
-        if(null==webPage){
+        if (null == webPage) {
             return;
         }
         String regex = ".*\\：|\\s*";
@@ -101,40 +103,40 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
         //市盈率(静态)
         String sjljt = tds1.get(4).text().replaceAll(regex, "");
         //5 营业总收入
-        String yyzsr=tds1.get(5).text();
+        String yyzsr = tds1.get(5).text();
         //市净率
         String sjl = tds1.get(8).text().replaceAll(regex, "");
         //9 净利润
-        String jlr=tds1.get(9).text();
+        String jlr = tds1.get(9).text();
         //总市值
         String zsz = tds1.get(11).text().replaceAll("\\D+", "");
         //每股净资产
-        double mgjzc = MathFormat.doubleFormat(tds1.get(12).text().replaceAll("\\[.*|", ""));
+        Double mgjzc = MathFormat.doubleFormat(tds1.get(12).text().replaceAll("\\[.*|", ""));
         //毛利率
-        Double mll =MathFormat.parseMath(tds1.get(13).getElementsByClass("tip f12").text());
+        Double mll = MathFormat.parseMath(tds1.get(13).getElementsByClass("tip f12").text());
         String jzcsyl = "-1";
         if (tds1.size() > 14) {
             //净资产收益率
             jzcsyl = tds1.get(14).select("span").get(1).text();
         }
         //提取正负小数正则
-        String rege_number="[-+]?([0]{1}(\\.[0-9]+)?|[1-9]{1}\\d*(\\.[0-9]+)?)";
+        String rege_number = "[-+]?([0]{1}(\\.[0-9]+)?|[1-9]{1}\\d*(\\.[0-9]+)?)";
         //关键字替换为- 负数是用中文表示所以得转换
-        String down_flag="同比下降";
+        String down_flag = "同比下降";
         //净利润
-        jlr=jlr.replaceAll("\\[.*|\\s|\\：","").replace(down_flag,"-").replace("未公布","0,");
+        jlr = jlr.replaceAll("\\[.*|\\s|\\：", "").replace(down_flag, "-").replace("未公布", "0,");
         //营业总收入
-        yyzsr=yyzsr.replaceAll("\\[.*|\\s|\\：","").replace(down_flag,"-").replace("未公布","0,");
-        String jlr_data[]=StockSpider.findStrByRegx(jlr,rege_number).split(",");
-        String yyzsr_data[]=StockSpider.findStrByRegx(yyzsr,rege_number).split(",");
+        yyzsr = yyzsr.replaceAll("\\[.*|\\s|\\：", "").replace(down_flag, "-").replace("未公布", "0,");
+        String jlr_data[] = StockSpider.findStrByRegx(jlr, rege_number).split(",");
+        String yyzsr_data[] = StockSpider.findStrByRegx(yyzsr, rege_number).split(",");
 
-        String totalProfits=jlr_data.length>0?jlr_data[0]:"0";
-        String profitsDiff=jlr_data.length>1?jlr_data[1]:"0";
-        String totalIncome=yyzsr_data.length>0?yyzsr_data[0]:"0";
-        String incomeDiff=yyzsr_data.length>1?yyzsr_data[1]:"0";
+        String totalProfits = jlr_data.length > 0 ? jlr_data[0] : "0";
+        String profitsDiff = jlr_data.length > 1 ? jlr_data[1] : "0";
+        String totalIncome = yyzsr_data.length > 0 ? yyzsr_data[0] : "0";
+        String incomeDiff = yyzsr_data.length > 1 ? yyzsr_data[1] : "0";
         //报告期
-        Elements element =doc.getElementsByAttributeValue("style","margin-top: 4px;margin-right: 10px;color:#666");
-        String report_date=null==element?"":element.text().trim().replace("以上为","");
+        Elements element = doc.getElementsByAttributeValue("style", "margin-top: 4px;margin-right: 10px;color:#666");
+        String report_date = null == element ? "" : element.text().trim().replace("以上为", "");
 
         /*//xueqiu baseinfo
         XqQuoteDto dto=new XqQuoteDto();
@@ -156,7 +158,6 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
         }*/
 
 
-
         UpdateResult updateResult = getMongoTemp().upsert(
                 new Query(Criteria.where("_id").is(currentCodeInfo.getCode())),
                 new Update()
@@ -169,13 +170,13 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
                         .set("bvps", mgjzc)
                         .set("pes", MathFormat.doubleFormat(sjljt))
                         .set("ped", MathFormat.doubleFormat(dtsyl))
-                        .set("totalProfits",Double.parseDouble(totalProfits))
-                        .set("profitsDiff",Double.parseDouble(profitsDiff))
-                        .set("totalIncome",Double.parseDouble(totalIncome))
-                        .set("incomeDiff",Double.parseDouble(incomeDiff))
-                        .set("report",report_date)
-                        .set("stype",stype) //股票分类
-                        .set("mll",mll)
+                        .set("totalProfits", Double.parseDouble(totalProfits))
+                        .set("profitsDiff", Double.parseDouble(profitsDiff))
+                        .set("totalIncome", Double.parseDouble(totalIncome))
+                        .set("incomeDiff", Double.parseDouble(incomeDiff))
+                        .set("report", report_date)
+                        .set("stype", stype) //股票分类
+                        .set("mll", mll)
 //                        .set("pettm",dto.getPe_ttm())
 //                        .set("high52w",dto.getHigh52w())
 //                        .set("low52w",dto.getLow52w())
@@ -186,13 +187,11 @@ public class ThsInfoSpider extends AbstractHtmlSpider<Stock> {
         );
         UpdateResult updateResult2 = getMongoTemp().upsert(
                 new Query(Criteria.where("_id").is(currentCodeInfo.getCode())),
-                new Update().set("infoDate", Integer.valueOf(TradingDateUtil.getDateYYYYMMdd())),"stock_code_info");
-        log.info("代码{}受影响行数:{}",currentCodeInfo.getCode(),updateResult.getModifiedCount()+updateResult2.getModifiedCount());
-
+                new Update().set("infoDate", Integer.valueOf(TradingDateUtil.getDateYYYYMMdd())), "stock_code_info");
+        log.info("代码{}受影响行数:{}", currentCodeInfo.getCode(), updateResult.getModifiedCount() + updateResult2.getModifiedCount());
 
 
     }
-
 
 
 }
